85a7ed9e27bb01329448387e820d80a66e76230a
Rafael Vareto <rafaelvareto@gmail.com>
2016-07-09 10:40:07 -0300

    Adding Probe set

---
+++ QuisCampi/script.sh
@@ -0,0 +1,3 @@
+
+# rsync -rv --include '*/' --include '*.js' --exclude '*' --prune-empty-dirs Source/ Target/
+rsync -rv --include '*/' --include '*.jpg' --exclude '*' --prune-empty-dirs /home/rafael/STORAGE/datasets/DATASETS/quiscamp/qcDatasetRep/ /home/rafael/Downloads/QC-imagesOnly/
